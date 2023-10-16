35b2c0201930bbcdddf472d7682f9bc59f1e2bde
FelipeSenaC94 <senamoving@gmail.com>
2023-10-15 22:00:05 -0300

    primeiro

---
+++ FilmesApi/Data/Dtos/CreateFilmeDto.cs
@@ -0,0 +1,24 @@
+﻿using System.ComponentModel.DataAnnotations;
+
+namespace FilmesApi.Data.Dtos;
+
+public class CreateFilmeDto
+{
+
+
+
+
+    [Required(ErrorMessage = "O título é obrigatório")]
+    public string Titulo { get; set; }
+
+
+
+    [Required(ErrorMessage = "O gênero é obrigatório")]
+    [StringLength(100, ErrorMessage = "O numero max de caracteres é 100")]
+    public string Genero { get; set; }
+
+
+    [Required(ErrorMessage = "A duração é obrigatória")]
+    [Range(70, 600, ErrorMessage = "A duração precisa ser entre 70 e 600 minutos")]
+    public int Duracao { get; set; }
+}
